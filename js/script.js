4e1f17b6deaed0ae829ee445b83c0a2b2456af34
Melrizzuto <melrizzuto4@gmail.com>
2024-11-19 16:03:58 +0100

    created cards js

--- js/script.js
+++ js/script.js
@@ -15,3 +15,27 @@ const params = { "_limit": 6 };
 axios.get(baseUrl + resource, { params }).then((res) => {
     console.log(res.data);
 })
+
+// creo le cards da js
+let postcards = document.querySelector(".container");
+let numberOfCards = 5;
+
+for (let i = 0; i < numberOfCards; i++) {
+    // devo creare il template delle cards e inserire 6 figures dentro card
+    let card = document.createElement("div");
+    card.classList.add("row")
+    card.innerHTML += `
+     <div class="col">
+            <div id="pin">
+                <img src="./img/pin.svg" alt="">
+            </div>
+            <figure>
+                <img src="./img/wall.png" alt="Trulli" style="width:100%">
+        <figcaption>Fig.1 - Trulli, Puglia, Italy</figcaption>
+            </figure>
+            </div>
+        `;
+    postcards.appendChild(card);
+
+    console.log(card);
+}
